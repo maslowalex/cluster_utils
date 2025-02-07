306cee0bb552b92060af80f05354aafef0ed170f
Olexandr Maslo <maslo.rails@gmail.com>
2025-02-07 20:45:25 +0200

    threads, unacceptable performance

--- src/bin/get_trades.rs
+++ src/bin/get_trades.rs
@@ -3,6 +3,9 @@ use cluster_utils::bybit;
 use cluster_utils::common::{Cluster, Trade};
 use trade_aggregation::*;
 
+use std::sync::Arc;
+use std::thread;
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Arguments {
@@ -20,33 +23,59 @@ fn main() {
     let args = Arguments::parse();
 
     let trades = bybit::get_trades(&args.symbol, args.days_ago);
+    println!("Total number of trades in batch: {}", trades.len());
 
+    let trades = Arc::new(trades);
+
+    let timeframes = vec![300, 600, 1800, 3600, 14400];
+    let timeframes = Arc::new(timeframes);
+
+    let handles: Vec<_> = (0..timeframes.len())
+        .map(|timeframe: usize| {
+            let data_clone = Arc::clone(&trades);
+            let timeframes = Arc::clone(&timeframes);
+            let symbol = args.symbol.clone();
+            thread::spawn(move || {
+                aggregate_trades(data_clone, timeframes[timeframe], &symbol, args.days_ago);
+            })
+        })
+        .collect();
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
+
+fn aggregate_trades(trades: Arc<Vec<Trade>>, timeframe: i64, symbol: &str, days_ago: i32) -> () {
     // specify the aggregation rule to be time based and the resolution each trade timestamp has
-    let time_rule = TimeRule::new(args.period_sec, TimestampResolution::Millisecond);
+    let time_rule = TimeRule::new(timeframe, TimestampResolution::Millisecond);
     // Notice how the aggregator is generic over the output candle type,
     // the aggregation rule as well as the input trade data
     let mut aggregator = GenericAggregator::<Cluster, TimeRule, Trade>::new(time_rule, true);
 
     let mut clusters: Vec<Cluster> = Vec::new();
 
-    for t in trades {
-        if let Some(cluster) = aggregator.update(&t) {
-            clusters.push(cluster);
+    let start = std::time::Instant::now();
+    for t in trades.iter() {
+        if let Some(mut cluster) = aggregator.update(&t) {
+            cluster.finalize();
 
-            dbg!(&clusters.len());
+            clusters.push(cluster);
         }
     }
 
-    clusters.iter_mut().for_each(|c| c.finalize());
+    let elapsed = start.elapsed();
+    println!("Final: {:?}", elapsed);
 
-    let ten: Vec<Cluster> = clusters.into_iter().collect();
-
-    let json = serde_json::to_string(&ten).unwrap();
+    println!("Clusters: {}", clusters.len());
+    println!("Generating JSON");
+    let json = serde_json::to_string(&clusters).unwrap();
+    println!("Writing to file");
 
     // write to file
     let file_name = format!(
         "data/{}-{}-{}dago-clusters.json",
-        args.symbol, args.period_sec, args.days_ago
+        symbol, timeframe, days_ago
     );
     std::fs::write(file_name, json).unwrap();
 }
